eb55a7a699ea08343a2a622c4fd6066c0ffe40ad
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-06-30 19:54:05 +0300

    Added Redux Provider and store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
 
 // Import components
 import Posts from "./components/Posts";
 import PostForm from "./components/Postform";
 
+// Import store
+import store from "./store";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
