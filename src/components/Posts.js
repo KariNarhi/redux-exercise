16e38940fb0e49eb59a2c2757ff1c122e4ebdf6e
Kari Narhi <Kari.Narhi24@hotmail.com>
2020-07-03 16:19:52 +0300

    Removed non-relevant old code

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -29,12 +29,10 @@ class Posts extends Component {
 Posts.propTypes = {
   fetchPosts: PropTypes.func.isRequired,
   posts: PropTypes.array.isRequired,
-  newPost: PropTypes.object,
 };
 
 const mapStateToProps = (state) => ({
   posts: state.posts.items,
-  newPost: state.posts.item,
 });
 
 export default connect(mapStateToProps, { fetchPosts })(Posts);
